9cb13a054361a2f871c2083fd2ee96d611524ef2
picrack <75869749+picrack@users.noreply.github.com>
2020-12-16 05:11:49 -0300

    Update proveedor.py

--- models/proveedor.py
+++ models/proveedor.py
@@ -1,9 +1,9 @@
  # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+#from odoo import models, fields, api
 
-class Proveedor(models.Model):
-	_name = 'inventario.proveedor'
+#class Proveedor(models.Model):
+#	_name = 'inventario.proveedor'
 
-	name = fields.Char(string = "Nombre Proveedor", requiered = True)
-	codigo_proveedor = fields.Integer(string = "Codigo proveedor", required = True)
+#	name = fields.Char(string = "Nombre Proveedor", requiered = True)
+#	codigo_proveedor = fields.Integer(string = "Codigo proveedor", required = True)
