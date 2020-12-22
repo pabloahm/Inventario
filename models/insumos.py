b0b4c9faaa0fd1e3094f62213ab6b572308337af
picrack <75869749+picrack@users.noreply.github.com>
2020-12-22 05:51:48 -0300

    Update insumos.py
    carga 3

--- models/insumos.py
+++ models/insumos.py
@@ -7,6 +7,18 @@ class TipoInsumos(models.Model):
 
 	name = fields.Char(string = "Nombre", required=True)
 
+#class Proveedor(models.Model):
+#	_name = 'inventario.proveedor'
+
+#	name = fields.Char(string = "Nombre Proveedor", requiered = True)
+#	codigo_proveedor = fields.Integer(string = "Codigo proveedor", required = True)
+
+#    insumos_ids = fields.One2many(
+#		'inventario.insumos'
+#		'proveedor_id'
+#		string="Proveedor"
+#	)
+
 
 class Insumos(models.Model):
 	_name = 'inventario.insumos'
@@ -20,3 +32,6 @@ class Insumos(models.Model):
 
 	tipo_insumos_id = fields.Many2one(
 		'inventario.tipo_insumos', string="Tipo de Insumo")
+	#proveedor_id = fields.Many2one(
+	#	'inventario.proveedor', string="Codigo Proveedor", required =True
+	#)
